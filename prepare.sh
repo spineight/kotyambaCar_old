08937175d36bc13197f9c500fbf9f76954484aa1
spineight <korololeg@gmail.com>
2019-12-02 23:10:21 -1000

    prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -7,10 +7,10 @@ echo "preparing environment"
 sudo apt install python-pip
 pip --version
 pip install virtualenv
-echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
-. ~/.bashrc
-virtualenv env
-. /env/bin/activate
+#echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+#. ~/.bashrc
+#virtualenv env
+#. /env/bin/activate
 
 echo "Using systemd to setup starting of Tornado web server on boot"
 sudo cp src/control_server/start_server.service /etc/systemd/system
