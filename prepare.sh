16d71c90cd3fab6217fb19f2f2bd7cc3e46accc6
spineight <korololeg@gmail.com>
2019-12-02 21:22:05 -1000

    Added preparation script

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,19 @@
 echo "Preparing kotyambaCar"
 
-export kotyambaCar_path $(pwd)
+export kotyambaCar_path="$(pwd)"
 echo "Settting kotyambaCar_path to $kotyambaCar_path"
 
+echo "preparing environment"
+sudo apt install python-pip
+pip --version
+pip install virtualenv
+echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+. ~/.bashrc
+virtualenv env
+. /env/bin/activate
+
 echo "Using systemd to setup starting of Tornado web server on boot"
-sudo cp src/control_motors/start_server.service /etc/systemd/system
+sudo cp src/control_server/start_server.service /etc/systemd/system
 sudo systemctl daemon-reload
 # make service running on system boot:  
 sudo systemctl enable start_server.service --now
