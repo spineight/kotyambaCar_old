febce9694f4237bd2d2c8587ea222ab82080b8cc
spineight <korololeg@gmail.com>
2019-12-03 22:07:15 -1000

    ROS ip

--- src/catkin-ws/src/kotyambaCar/scripts/setup_master_node.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_master_node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "SETTING UP Master Node IP"
-export ROS_MASTER_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }'"
+export ROS_MASTER_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
 echo "ROS_MASTER_IP: $ROS_MASTER_IP"
 export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
 echo "ROS_MASTER_URI $ROS_MASTER_URI"
