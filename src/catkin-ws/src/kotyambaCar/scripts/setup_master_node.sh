c7b756539fdcdcbb21b2851ad869f7d904f51cbf
spineight <korololeg@gmail.com>
2019-12-03 19:38:07 -1000

    scripts for setting up ROS nodes IP

---
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_master_node.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ export ROS_MASTER_URI=http://commandCenter:11311
+ export ROS_IP=commandCenter
---
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_slave_node.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ export ROS_MASTER_URI=http://commandCenter.local:11311
+ export ROS_IP=raspberrypi.local
