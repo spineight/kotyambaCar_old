73e461341fd527012d67abd031d9002730133afc
spineight <korololeg@gmail.com>
2019-12-03 20:37:33 -1000

    Setup ROS nodes IP

--- src/catkin-ws/src/kotyambaCar/scripts/setup_master_node.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_master_node.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
- export ROS_MASTER_URI=http://commandCenter:11311
- export ROS_IP=commandCenter
+export ROS_MASTER_IP="$(getent hosts commandCenter.local | awk '{ print $1 }')"
+export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
+export ROS_IP="$ROS_MASTER_IP"
--- src/catkin-ws/src/kotyambaCar/scripts/setup_slave_node.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_slave_node.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
- export ROS_MASTER_URI=http://commandCenter.local:11311
- export ROS_IP=raspberrypi.local
+export ROS_MASTER_IP="$(getent hosts commandCenter.local | awk '{ print $1 }')"
+echo "ROS_MASTER_IP: $ROS_MASTER_IP"
+export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
+echo "ROS_MASTER_URI: $ROS_MASTER_URI"
+export ROS_IP="$(getent hosts raspberrypi.local | awk '{ print $1 }')"
+echo "ROS_IP $ROS_IP"
