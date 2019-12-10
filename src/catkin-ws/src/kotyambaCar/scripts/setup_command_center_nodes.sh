38ac39352e2d659b9652fb83db5a9afc575aaf24
spineight <korololeg@gmail.com>
2019-12-09 23:19:36 -1000

    setup raspberry as master server

--- src/catkin-ws/src/kotyambaCar/scripts/setup_command_center_nodes.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_command_center_nodes.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-echo "SETTING UP Master Node IP"
-export ROS_MASTER_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
+export ROS_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
+echo "ROS_IP $ROS_IP"
+echo "SETTING UP ROS_MASTER_IP to RaspberryPi IP"
+export ROS_MASTER_IP="$ROS_IP"
 echo "ROS_MASTER_IP: $ROS_MASTER_IP"
 export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
 echo "ROS_MASTER_URI $ROS_MASTER_URI"
-export ROS_IP="$ROS_MASTER_IP"
-echo "ROS_IP $ROS_IP"
 
 echo "Allow ssh to RaspberryPI"
 export ROSLAUNCH_SSH_UNKNOWN=1
+
+exec "$@"
