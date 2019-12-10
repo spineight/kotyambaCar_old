07b18d6aac0c047231eb09cffd45321d8732710a
spineight <korololeg@gmail.com>
2019-12-09 21:38:59 -1000

    .

--- prepare_command_center.sh
+++ prepare_command_center.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "This script should be run on command_center machine"
-echo "This script should be sourced"
-echo "setting path for kotyambaCar repo folders on Raspberry, will be used by ROS  *.launch files"
+echo "This script should be sourced\n\n"
+echo "setting path for kotyambaCar repo folders on Raspberry, will be used by ROS  *.launch files\n\n"
 
-read -p "Enter fullpath to kotyambaCar repo " fullpath
+read -p "Enter fullpath to kotyambaCar repo: " fullpath
 
 export KOTYAMBA_REPO_FOLDER_RASPBERRY="$fullpath"
+echo "KOTYAMBA_REPO_FOLDER_RASPBERRY was set to: $KOTYAMBA_REPO_FOLDER_RASPBERRY"
+echo "check it!"
