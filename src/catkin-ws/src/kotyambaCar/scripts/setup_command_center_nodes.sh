e23b6fca2ed70b2c4856cf21dfcfa2afa9dd2b28
spineight <korololeg@gmail.com>
2019-12-10 02:49:39 -1000

    raspberrypi to master

--- src/catkin-ws/src/kotyambaCar/scripts/setup_command_center_nodes.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_command_center_nodes.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
+source /opt/ros/melodic/setup.bash
+source /home/oleg/dev/kotyambaCar/src/catkin-ws/devel/setup.bash
+
 echo "SETTING UP Master Node IP"
-export ROS_MASTER_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
+export ROS_MASTER_IP="$(getent ahosts raspberrypi.local | awk 'NR==1{ print $1 }')"
 echo "ROS_MASTER_IP: $ROS_MASTER_IP"
 export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
 echo "ROS_MASTER_URI $ROS_MASTER_URI"
-export ROS_IP="$ROS_MASTER_IP"
+export ROS_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
 echo "ROS_IP $ROS_IP"
 
-echo "Allow ssh to RaspberryPI"
-export ROSLAUNCH_SSH_UNKNOWN=1
+exec "$@"
--- src/catkin-ws/src/kotyambaCar/scripts/setup_raspberry_pi_nodes.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_raspberry_pi_nodes.sh
@@ -3,7 +3,7 @@
 source /home/pi/ros_catkin_ws/install_isolated/setup.bash
 source /home/pi/kotyambaCar/src/catkin-ws/devel/setup.bash
 export KOTYAMBA_PATH=/home/pi/kotyambaCar
-export ROS_MASTER_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
+export ROS_MASTER_IP="$(getent ahosts raspberrypi.local | awk 'NR==1{ print $1 }')"
 echo "ROS_MASTER_IP: $ROS_MASTER_IP"
 export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
 echo "ROS_MASTER_URI: $ROS_MASTER_URI"
