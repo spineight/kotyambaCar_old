bcadd0976baa60c642c5d3834e3bc7a1bc2f09f9
spineight <korololeg@gmail.com>
2019-12-10 01:14:28 -1000

    fix master_ip and uri for command center

--- src/catkin-ws/src/kotyambaCar/scripts/setup_command_center_nodes.sh
+++ src/catkin-ws/src/kotyambaCar/scripts/setup_command_center_nodes.sh
@@ -4,7 +4,8 @@ source ~/.bashrc
 export ROS_IP="$(getent ahosts commandCenter.local | awk 'NR==1{ print $1 }')"
 echo "ROS_IP $ROS_IP"
 
-echo "Allow ssh to RaspberryPI"
-export ROSLAUNCH_SSH_UNKNOWN=1
+export ROS_MASTER_IP="$(getent ahosts raspberrypi.local | awk 'NR==1{ print $1 }')"
+echo "ROS_MASTER_IP: $ROS_MASTER_IP"
+export ROS_MASTER_URI="http://$ROS_MASTER_IP:11311"
 
 exec "$@"
