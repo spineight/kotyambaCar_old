030850b122eb3a6bb0717451002d7989c4bf2649
spineight <korololeg@gmail.com>
2019-12-14 05:58:40 -1000

    modes

--- src/control_server/driving_modes_manager.py
+++ src/control_server/driving_modes_manager.py
@@ -2,6 +2,8 @@
 # http://wiki.ros.org/roslaunch/API%20Usage
 import roslaunch
 
+from network_status import get_command_center_ip
+
 class DrivingModesManager():
   def __init__(self):
     self.active_launch_file = None
@@ -10,8 +12,24 @@ class DrivingModesManager():
       if(self.active_launch_file is not None):
         self.active_launch_file.shutdown()
         print "terminating {} mode".format(self.active_mode_name) 
+  
   def start_manual_mode(self):
     self.terminate_active_mode()
+    self.active_mode_name="maunal mode"
+    print "starting {}".format(self.active_mode_name)
+    try:
+      get_command_center_ip()
+    except Exception as e:
+      print e
+      print "starting without camera broadcasting"
+    else:
+      print "started manual mode with camera broadcasting"
+      uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+      roslaunch.configure_logging(uuid)
+      self.active_launch_file = roslaunch.parent.ROSLaunchParent(uuid, ["../catkin-ws/src/kotyambaCar/launch/manual_mode_with_camera.launch"])
+      self.active_launch_file.start()
+      rospy.loginfo("started manual mode with camera broadcasting")
+    
   def start_training_mode(self):
     self.terminate_active_mode()
   
--- src/control_server/network_status.py
+++ src/control_server/network_status.py
@@ -6,6 +6,7 @@ def get_command_center_ip():
     command_center_ip = subprocess.check_output(["getent", "ahosts", "commandCenter.local"]).split()[0]
     return command_center_ip
   except Exception as e:
+    print e
     # https://realpython.com/python-exceptions/
     raise Exception("commandCenter.local not available")
   
