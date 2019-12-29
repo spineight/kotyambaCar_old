16843bed852b25c316d3f146f6bb952e66256643
spineight <korololeg@gmail.com>
2019-12-29 12:20:49 -1000

    .

--- src/control_motors/cli_manual_mode.py
+++ src/control_motors/cli_manual_mode.py
@@ -13,7 +13,7 @@ def main():
 
   try:
     while(True):
-      print "Cmd format:****** %SPEED,%STEER,TIME_SEC, freqForSteeringMotor******"
+      print "Cmd format:****** %SPEED,%STEER,TIME_SEC******"
       print "speed in [-100..100] backward..forward"
       print "steer in [-100..100] left..right"
       print "\n\n\nFor ex.: '80,90,3' - move forward 80% of max power, right 90% of max power for 3 seconds"
@@ -26,9 +26,8 @@ def main():
       if(cmdStr in "s"):
         speed_dc, steer_dc, active_time = [0,0,0]
       else:
-        speed_dc, steer_dc, active_time, freq_steering = (float(v) for v in cmdStr.split(','))
+        speed_dc, steer_dc, active_time = (float(v) for v in cmdStr.split(','))
 
-      SteerControlMotor.speedPWM.ChangeFrequency(freq_steering)
       if(speed_dc >= 0):
         car.moveForwardAsync(speed_dc, steer_dc, active_time)
       else:
